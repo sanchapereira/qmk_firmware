e81e48ec946e2e654a6145d84e07fb8aaed346d1
Sancha Pereira <sancha.pereira@onfido.com>
2019-09-27 11:35:58 +0100

    Add home & end

--- keyboards/keebio/iris/keymaps/sanchapereira/keymap.c
+++ keyboards/keebio/iris/keymaps/sanchapereira/keymap.c
@@ -25,11 +25,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                              KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_CAPS,   KC_NO,   KC_NO,   KC_MRWD, KC_MPLY, KC_MFFD,                           KC__MUTE, KC__VOLDOWN, KC__VOLUP, KC_NO, KC_MINS, KC_EQL,
+     KC_NO,   KC_NO,   KC_NO,   KC_MRWD, KC_MPLY, KC_MFFD,                           KC__MUTE, KC__VOLDOWN, KC__VOLUP, KC_NO, KC_MINS, KC_EQL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                              KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_GRV,  KC_BSLS,
+     KC_CAPS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                              KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_GRV,  KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+     KC_LSFT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_HOME, KC_END,  KC_NO,   KC_NO,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘         \       ┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_NO,   KC_NO,   KC_NO,                     KC_NO,   KC_RSFT,  KC_NO
                                 // └────────┴────────┴────────┘                 \────────┴────────┴────────┘
